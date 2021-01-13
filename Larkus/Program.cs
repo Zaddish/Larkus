74a3c0ca7009886d3540c947d818aab1c4501a65
Zaddish <40848785+Zaddish@users.noreply.github.com>
2021-01-13 13:27:13 +1100

    Add files via upload

---
+++ Larkus/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Larkus
+{
+    class Program
+    {
+        static void Main(string[] args){
+            Console.WriteLine("Welcome to your control panel!");
+
+        sendCmd:
+            sendCmd(Console.ReadLine());
+            //Console.ReadKey();
+            goto sendCmd;
+        }
+
+        public static void sendCmd(string cmd)
+        {
+            Socket s = new Socket(AddressFamily.InterNetworkV6, SocketType.Dgram, ProtocolType.Udp);
+            s.SetSocketOption(SocketOptionLevel.IPv6, SocketOptionName.IPv6Only, false);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
+            Byte[] data = Encoding.ASCII.GetBytes(cmd);
+            s.SendTo(data, ipep);
+
+
+        }
+
+    }
+    
+}
